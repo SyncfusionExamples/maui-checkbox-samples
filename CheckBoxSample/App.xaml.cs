0f64c7057dcc5fcbe41e65392b42b60fd2c30e54
Brundha <brundha.sf3969@gmail.com>
2024-08-14 18:12:36 +0530

    Added the CheckBoxSample.

---
+++ CheckBoxSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckBoxSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
